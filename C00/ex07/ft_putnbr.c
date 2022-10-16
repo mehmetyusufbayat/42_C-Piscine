35dcd57df56787e04f54e70aec847e1b112c4c85
mehmetyusufbayat <bayatmehmetyusuf@gmail.com>
2022-10-16 12:20:01 +0300

    ex07

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mebayat <mebayat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/25 20:01:08 by mebayat           #+#    #+#             */
+/*   Updated: 2022/08/25 20:38:59 by mebayat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar(45);
+		ft_putchar(50);
+		nb = 147483648;
+	}
+	if (nb < 0)
+	{
+		ft_putchar(45);
+		nb = -nb;
+	}
+	if (nb < 10)
+	{
+		ft_putchar(nb + 48);
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
