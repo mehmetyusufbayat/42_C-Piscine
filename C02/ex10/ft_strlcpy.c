11f2d922b6885fbec417c651f517b01b6e98910e
mehmetyusufbayat <bayatmehmetyusuf@gmail.com>
2022-11-14 10:01:31 +0300

    ex10

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mebayat <mebayat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/01 19:05:57 by mebayat           #+#    #+#             */
+/*   Updated: 2022/09/02 19:02:52 by mebayat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	count;
+
+	i = 0;
+	count = 0;
+	while (src[count] != '\0')
+		count++;
+	if (size != 0)
+	{
+		while (src[i] != '\0' && (i < size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	}
+	dest[i] = '\0';
+	return (count);
+}
