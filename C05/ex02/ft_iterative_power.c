4be95f29b6c616a4ea61865d50c465322c0818eb
mehmetyusufbayat <bayatmehmetyusuf@gmail.com>
2023-07-13 08:41:32 +0300

    ex02

---
+++ C05/ex02/ft_iterative_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mebayat <mebayt@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 11:38:37 by mebayat           #+#    #+#             */
+/*   Updated: 2022/09/07 13:19:25 by mebayat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	p;
+
+	if (power < 0)
+		return (0);
+	else if (power == 0)
+		return (1);
+	p = nb;
+	while (power > 1)
+	{
+		p = p * nb;
+		power--;
+	}
+	return (p);
+}
