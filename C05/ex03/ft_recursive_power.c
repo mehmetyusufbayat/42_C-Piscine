bec75bcd323f1cd1e2139d3801b3f0d72ee34cd6
mehmetyusufbayat <bayatmehmetyusuf@gmail.com>
2023-07-13 08:44:15 +0300

    ex03

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mebayat <mebayat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 13:39:36 by mebayat           #+#    #+#             */
+/*   Updated: 2022/09/07 13:44:50 by mebayat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	else if (power == 0)
+		return (1);
+	return (nb * (ft_recursive_power(nb, (power - 1))));
+}
